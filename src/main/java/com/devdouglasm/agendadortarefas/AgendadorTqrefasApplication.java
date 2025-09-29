6053e57e0b40df7e9d9a50d06ff0f986aa27332c
devdouglasm <devdouglasm@gmail.com>
2025-09-29 11:16:10 -0300

    commit inicial

---
+++ src/main/java/com/devdouglasm/agendadortarefas/AgendadorTqrefasApplication.java
@@ -0,0 +1,13 @@
+package com.devdouglasm.agendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTqrefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTqrefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdouglasm/agendadortarefas/AgendadorTerefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdouglasm.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTerefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
