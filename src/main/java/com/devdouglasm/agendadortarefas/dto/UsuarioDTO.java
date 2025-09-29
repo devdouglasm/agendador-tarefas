d411e286ddc04bdd44d359e400e1c8d0c8e5a2dd
devdouglasm <devdouglasm@gmail.com>
2025-09-29 11:50:02 -0300

    config security adicionadas

---
+++ src/main/java/com/devdouglasm/agendadortarefas/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.devdouglasm.agendadortarefas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
---
+++ src/main/java/com/devdouglasm/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -0,0 +1,16 @@
+package com.devdouglasm.agendadortarefas.infrastructure.client;
+
+import com.devdouglasm.agendadortarefas.dto.UsuarioDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "usuario", url = "${usuario.url}")
+public interface UsuarioClient {
+
+    @GetMapping
+    UsuarioDTO achaUsuario(@RequestParam String email,
+                           @RequestHeader("Authorization") String token);
+
+}
